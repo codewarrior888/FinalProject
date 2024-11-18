a202fca6f29fd0751f3cd41d71f695e5d5005366
Kirill Skakov <kirill.skakov@gmail.com>
2024-11-18 11:37:48 +0900

    tabs style for mobile

--- frontend/src/components/Main/MainAuthenticated.tsx
+++ frontend/src/components/Main/MainAuthenticated.tsx
@@ -1,35 +1,39 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { useAuth } from "../Authenticate/useAuth";
-import "../../styles/MainAuthenticated.scss";
 import Equipment from "../TableTabs/Equipment";
 import Maintenance from "../TableTabs/Maintenance";
 import Claim from "../TableTabs/Claim";
 import RoleLabel from "../Authenticate/RoleLabel";
+import "../../styles/MainAuthenticated.scss";
 
 const MainAuthenticated = () => {
   return (
     <div className="main-authenticated">
       <div className="main-authenticated__container">
         <Tabs>
-          <TabList>
-            <Tab>Общая информация</Tab>
-            <Tab>ТО</Tab>
-            <Tab>Рекламации</Tab>
-          </TabList>
+          <div className="tabs-container">
+            <TabList>
+              <Tab>Общая информация</Tab>
+              <Tab>ТО</Tab>
+              <Tab>Рекламации</Tab>
+            </TabList>
 
-          <TabPanel>
-            <RoleLabel />
-            <Equipment />
-          </TabPanel>
+            <div className="tab-content">
+              <TabPanel>
+                <RoleLabel />
+                <Equipment />
+              </TabPanel>
 
-          <TabPanel>
-            <RoleLabel />
-            <Maintenance />
-          </TabPanel>
-          <TabPanel>
-            <RoleLabel />
-            <Claim />
-          </TabPanel>
+              <TabPanel>
+                <RoleLabel />
+                <Maintenance />
+              </TabPanel>
+              
+              <TabPanel>
+                <RoleLabel />
+                <Claim />
+              </TabPanel>
+            </div>
+          </div>
         </Tabs>
       </div>
     </div>
